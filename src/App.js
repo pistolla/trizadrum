63fa1ecfa6e380d699d80627d9f602da951f71c2
Dataphile joe <pinkissme@gmail.com>
2020-06-10 18:38:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import  DrumPad from "./components/DrumPad/DrumPad"
+
+
 
 function App() {
+  const sounds = [
+    {
+      key: "Q",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+    },
+    {
+      key: "W",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+    },
+    {
+      key: "E",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+    },
+    {
+      key: "A",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+    },
+    {
+      key: "S",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+    },
+  
+    {
+      key: "D",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+    },
+    {
+      key: "Z",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+    },
+    {
+      key: "X",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    },
+    {
+      key: "C",
+      mp3: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+    }]
+  
+  const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine" className="container">
+        <div id="display" className="display">
+          <h1>play a sound</h1>
+          {sounds.map((sound, idx) => (<DrumPad text={sound.key} key={idx} audio={sound.mp3} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/DrumPad/DrumPad.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import './DrumPad.css';
+
+function DrumPad(props) {
+  const audio = useRef(props.audio);
+  useEffect((props) => {
+    // audio.current.addEventListener('ended', (e) => {
+    //   const parent = e.target.parentNode;
+    //   parent.classList.remove('active');
+    // });
+    document.addEventListener('keydown', (e) => {
+      const id = e.key.toUpperCase();
+      const audio = document.getElementById(id);
+      if (audio) {
+        audio.currenttime = 0;
+        const parent = audio.parentNode;
+        parent.classList.add('active');
+
+        const display = parent.parentNode;
+        display.querySelector('h1').innerText = `${id} is playing`; audio.play();
+      }
+
+    });
+
+  }, [props.audio]);
+
+  const playSound = () => {
+    props.audio.play();
+    const id = props.audio.id;
+    const parent = props.audio.parentNode;
+    parent.classList.add('active');
+    const display = parent.parentNode;
+    display.querySelector('h1').innerText = `${id} is playing`;
+
+  }
+
+  return (
+    <div className="drum-pad" onClick={playSound}
+      id={`drum-${props.text}`}>
+      {props.text}
+      <audio className="clip" id={props.text} >
+        <source src={props.audio} type="audio/mp3" />
+      </audio>
+    </div>
+  );
+}
+
+DrumPad.propTypes = {};
+
+DrumPad.defaultProps = {};
+
+export default DrumPad;
